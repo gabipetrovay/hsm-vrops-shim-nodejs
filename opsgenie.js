e820a727aba6d5026a936b93f85ca760ddbe2c57
Gabriel Petrovay <gabriel.petrovay@swisscom.com>
2017-04-12 12:08:27 +0200

    Disabled strinct SSL checks and added proxy setting if available in the environment

--- opsgenie.js
+++ opsgenie.js
@@ -4,16 +4,27 @@ var sdk = require('opsgenie-sdk');
 
 var apiKey = process.env.OPSGENIE_API_KEY;
 
+// exit the process if no OpsGenie API key is set
 if (!apiKey) {
     console.error('Please provide the OPSGENIE_API_KEY environment variable.');
     process.exit(1);
 }
 
-sdk.configure({
+// build the OpsGenie configuration object
+var config = {
     api_key: apiKey,
-    proxy: process.env.http_proxy
-});
+    strictSSL: false
+};
+if (process.env.http_proxy) {
+    config.proxy = 'http://' + process.env.http_proxy;
+}
 
+// configure OpsGenie SDK
+sdk.configure(config);
+
+/**
+ * Given a vROps alert object, create an OpsGenie alert.
+ */
 exports.vropsAlertToOpsGenieAlert = function (vropsAlert, callback) {
     debug('vROps alert:', JSON.stringify(vropsAlert));
 
@@ -27,7 +38,7 @@ exports.vropsAlertToOpsGenieAlert = function (vropsAlert, callback) {
     };
 
     sdk.alert.create(opsgenieAlert, function (err, alert) {
-        debug('Opsgenie alert:', JSON.stringify(alert));
+        debug('OpsGenie alert:', JSON.stringify(alert));
 
         return callback(err, alert); 
     });
